4de2e133a8746515a36abf9fd76179d209054fed
Brandon Eads <brandon181989@hotmail.com>
2021-09-08 14:06:34 -0700

    added lots of stuff

--- src/main.js
+++ src/main.js
@@ -1,7 +1,62 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import { ClassExample1, ClassExample2 } from './project';
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import BikeIndex from "./project";
 
+function clearFields() {
+  $("#city").val("");
+  $("#bike-search-output").empty();
+  $("show-errors").empty();
+}
 
+// function lastWeek(results) {
+//   results.bikes.forEach((bike) => {
+//     if (bike.date_stolen < Date.now() - 604800000 || bike.date_stolen === null) {
+//       results.bikes.splice(bike, 1); 
+//       console.log(bike.date_stolen);
+//     }
+//   });
+//   return results;
+// }
+
+function lastWeek(results) {
+  // let date = Date.now();
+  let bikeArray = results.bikes;
+  console.log(bikeArray);
+  // // bikeArray.forEach((bike, i) => {
+  // //   if (bike.date_stolen < (date - 604800000) / 1000) {
+  // //     // bikeArray.splice(i, 1); 
+  // //   }
+  // });
+  return bikeArray;
+}
+
+$(document).ready(function () {
+  $("#bike-search").submit(function (event) {
+    event.preventDefault();
+    let city = $("#city").val();
+    clearFields();
+    let promise = BikeIndex.searchArea(city);
+    promise.then(function (response) {
+      let body = JSON.parse(response);
+      console.log(body);
+      const bikeResults = lastWeek(body);
+      console.log(bikeResults);
+      bikeResults.forEach(
+        (bike) => {
+          let msDateValue = bike.date_stolen * 1000;
+          let date = new Date(msDateValue);
+          $("#bike-search-output").append(
+            `<p>Date Stolen: ${date.toDateString()}</p><p>Title: ${bike.title}</p><p>Serial #: ${bike.serial}</p><p>Link: <a href=${bike.url}>${bike.url}</a></p><br>`
+          );
+        },
+        function (error) {
+          $("#show-errors").text(
+            `There was an error processing your request: ${error}`
+          );
+        }
+      );
+    });
+  });
+});
--- src/project.js
+++ src/project.js
@@ -1,27 +1,32 @@
-export class ClassExample1 {
-  constructor() {
+// Build an app using the Bike Index API. Your app could list all the bikes that have been registered as stolen in a given location in the past week. Or it could display statistics - for instance, you could see which manufacturer is most frequently stolen in a given area.
 
-  }
+//use api to gather list of bikes in city
+//reject any that are older than 1 week from Date.now()
+//body.bikes[0].date_stolen > Date.now() - 604800000
+//output: bike.title bike.serial bike.url
 
-  function1() {
-
-  }
-
-  function2() {
-
-  }
-}
-
-export class ClassExample2 {
-  constructor() {
-
-  }
-
-  function1() {
-
-  }
-
-  function2() {
-
+export default class BikeIndex {
+  static searchArea(city) {
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://bikeindex.org/api/v3/search?page=1&per_page=100&location=${city}&distance=10&stolenness=proximity&access_token=${process.env.API_KEY}`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
+
+// function lastWeek(results) {
+//   results.bikes.forEach((bike) => {
+//     if (bike.date_stolen < Date.now() - 604800000) {
+//       results.bikes.splice(bike, 1);
+//     }
+//   });
+// }
